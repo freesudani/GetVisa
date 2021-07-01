23c56f83668c6caccd0a3c9e4216672172b97121
freesudani <mohamed.dahiya@hotmail.com>
2021-07-01 19:03:19 +0700

    InfoSection

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  LightBg: false,
+  primary: true,
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "We help Buinsess Owner to inceasre our team of unique expecialitest",
+  headLine: "Lead Generation Specialist for Onl;ine Businesses",
+  lightText: true,
+  topLine: "Marketing Agency",
+  img: require("../../images/convertkit-htQznS-Rx7w-unsplash.jpg"),
+  alt: "image",
+  start: "",
+};
